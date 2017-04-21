b3166d46b0588187071a3f27ee3a41193ecfe7fa
Николай Николаевич Бобров <bk97w@bk.ru>
2017-04-21 20:29:50 +0300

    Add files via upload

--- adminGrid.js
+++ adminGrid.js
@@ -2,7 +2,16 @@ function funcBeforce() {
     $("#information").text("ожидание данных...");
 }
 
+
+
 function funcSuccess (data) {
+    //data = json.Parse(data);
+    var htmlCode = "<table><thead>";
+    htmlCode += "<td>id</td><td>last name</td><td>first name</td><td>email</td><td>password</td><td>role</td><td>subscription</td>";
+    htmlCode += "</thead>";
+    htmlCode += "<tr><td>data.lastName</td><td>data.fisrtName</td><td>data.email</td><td>data.email</td><td>data.password</td></tr>";
+    htmlCode += "</thead></table>";
+    //$("#information").html(htmlCode);
     $("#information").text(data);
 }
 
@@ -15,7 +24,7 @@ $(document).ready(function () {
                 name: "admin",
                 number: 3
             }),
-            dataType: "html",
+            dataType: "text",
             beforeSend: funcBeforce,
             success: funcSuccess
         });
--- content.php
+++ content.php
@@ -1,3 +1,9 @@
 <?php
-    echo $_POST['name'] . ' ' . $_POST['number'];
+$m = array('Honda','Hummer','BMW','Toyota');
+$s = array('AH','TU-144','Boing','apache');
+$t = array('tiger','panther','T-34','al ha');
+$k = array('dsad','sdad','sss','dasd');
+$THN = array($m, $s, $t, $k);
+$THN = json_encode($THN);
+echo $THN;
 ?>
