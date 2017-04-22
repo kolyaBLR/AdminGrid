b16a9cffdfabee83d3ac26feba43f00f7b70a235
Николай Николаевич Бобров <bk97w@bk.ru>
2017-04-22 04:33:36 +0300

    add table add table style

--- adminGrid.js
+++ adminGrid.js
@@ -1,30 +1,52 @@
 function funcBeforce() {
-    $("#information").text("ожидание данных...");
+    $("#entities-grid").text("ожидание данных...");
 }
 
+function tag(tag) {
+    return "<" + tag + ">";
+}
 
+function doubleTag(tag, value) {
+    return "<" + tag + ">" + value + "</" + tag + ">";
+}
 
 function funcSuccess (data) {
-    //data = json.Parse(data);
-    var htmlCode = "<table><thead>";
-    htmlCode += "<td>id</td><td>last name</td><td>first name</td><td>email</td><td>password</td><td>role</td><td>subscription</td>";
-    htmlCode += "</thead>";
-    htmlCode += "<tr><td>data.lastName</td><td>data.fisrtName</td><td>data.email</td><td>data.email</td><td>data.password</td></tr>";
-    htmlCode += "</thead></table>";
-    //$("#information").html(htmlCode);
-    $("#information").text(data);
+    var htmlCode = tag("table class='table table-bordered table-hover table-condensed'");
+    htmlCode += tag("thead");
+    htmlCode += tag("tr");
+    htmlCode += doubleTag("th", "id");
+    htmlCode += doubleTag("th", "last name");
+    htmlCode += doubleTag("th", "first name");
+    htmlCode += doubleTag("th", "email");
+    htmlCode += doubleTag("th", "password");
+    htmlCode += doubleTag("th", "role");
+    htmlCode += doubleTag("th", "subscription");
+    htmlCode += tag("/tr");
+    htmlCode += tag("/thead");
+    htmlCode += tag("tbody");
+    for (var i = 0; i < data.length; i++) {
+        htmlCode += tag("tr");
+        for (var j = 0; j < data[i].length; j++) {
+            htmlCode += doubleTag("td", data[i][j]);
+        }
+        htmlCode += tag("/tr");
+    }
+    htmlCode += tag("/tbody");
+    htmlCode += tag("/table");
+    $("#entities-grid").html(htmlCode);
 }
 
 $(document).ready(function () {
     $("#load").bind("click", function() {
         $.ajax ({
             url: "content.php",
-            type: "POST",
+            type: "GET",
             data: ({
-                name: "admin",
-                number: 3
+                page: 5,
+                filterbyfield: "title",
+                pattern: "smartphone"
             }),
-            dataType: "text",
+            dataType: "json",
             beforeSend: funcBeforce,
             success: funcSuccess
         });
